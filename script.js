f0569edfdddfcf34e596a70ef12474c877a962e9
De5how <78892416+De5how@users.noreply.github.com>
2021-12-03 21:30:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function deleteCase(thisCase) {
     thisCase.parentNode.parentNode.removeChild(thisCase.parentNode);
 }
 function clearCase(thisCase) {
+    thisCase.parentNode.setAttribute ("data-clear","cleared");
     thisCase.nextSibling.setAttribute("style", "text-decoration:line-through");
 }
 document.getElementById("submit").addEventListener("click", function (key, value){
@@ -13,10 +14,38 @@ document.getElementById("submit").addEventListener("click", function (key, value
         div.innerHTML = "<a onclick='clearCase(this);' class='success'>✓</a><div class='label'>"+input.value+"</div><a onclick='deleteCase(this);' class='deny'>✕</a>";
         list.append(div);
         input.value = "";
-        //localStorage.setItem('todo', list);
+//localStorage.setItem('todo', list);
     }
     else {
         alert('Заполните поле!');
         return false;
     }
 });
+
+
+function clear1() {
+    var listOf = document.getElementsByClassName('list');
+    for(var i=0;i<listOf.length;i++){
+        if(listOf[i].getAttribute ("data-clear")!="cleared"){
+            listOf[i].style.display = "none";
+        }else{
+            listOf[i].style.display = "flex";
+        }
+    }
+}
+function clear2() {
+    var listOf = document.getElementsByClassName('list');
+    for(var i=0;i<listOf.length;i++){
+        if(listOf[i].getAttribute ("data-clear")=="cleared"){
+            listOf[i].style.display = "none";
+        }else{
+            listOf[i].style.display = "flex";
+        }
+    }
+}
+function clear3() {
+    var listOf = document.getElementsByClassName('list');
+    for(var i=0;i<listOf.length;i++){
+        listOf[i].style.display = "flex";
+    }
+}
